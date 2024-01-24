517c0a7f1f84c8139d99fca380006e7d53d855d5
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-25 03:18:34 +0800

    Second commit, here I wrote a code where it will identify the tax to be paid adhering the given income.

--- exercise12.py
+++ exercise12.py
@@ -1,1 +1,21 @@
 #Exercise 12: Calculate income tax for the given income by adhering to the rules below
+
+income = 11000
+tax = 0
+
+if income <= 10000:
+    tax = 0
+elif income <= 20000:
+    i = income - 10000
+    tax = i * 0.10
+
+else:
+    
+    tax = 0
+    
+    tax = 10000 * 0.10
+    
+    tax += (income - 20000) * 0.20
+    
+print ("total of amount of tax to be paidd is:", tax)
+    
